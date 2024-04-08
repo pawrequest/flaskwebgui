76d2812136647676e04654790b622e81ab3937ca
paw <pawrequest@users.noreply.github.com>
2024-04-08 22:49:29 +0100

    spuirous '/' in find_browser_on_windows / brave

--- src/flaskwebgui.py
+++ src/flaskwebgui.py
@@ -89,7 +89,7 @@ def find_browser_on_mac():
 
 def find_browser_on_windows():
     paths = [
-        r"C:\Program Files\BraveSoftware\Brave-Browser\Application\\brave.exe",
+        r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe",
         r"C:\Program Files\Google\Chrome\Application\chrome.exe",
         r"C:\Program Files\Microsoft\Edge\Application\msedge.exe",
         r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
